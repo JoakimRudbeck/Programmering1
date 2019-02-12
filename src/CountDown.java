371804a8e72d263e3880c63d2580c4d49a246013
sbu470 <sbu470@edu.sollentuna.se>
2019-02-12 08:16:51 +0100

    update

--- src/CountDown.java
+++ src/CountDown.java
@@ -1,14 +1,35 @@
+import static java.lang.Thread.sleep;
+
 public class CountDown {
 
     public static void main(String[] args) throws InterruptedException {
-        countDown();
+        countDown(100, 16, 1000);
 
     }
 
-    private static void countDown() throws InterruptedException {
-        for(int i = 60*4; i >=0; i--){
-            System.out.println("Sekunder kvar till lektionsslut:" + Integer.toBinaryString(i));
-            Thread.sleep(1000);
+    private static void countDown(long minuterKvar, int bas, int millis) throws InterruptedException {
+        for(long i = 60*minuterKvar; i >= 0; i--){
+            switch(bas){
+                case 2:
+                    System.out.println(Long.toBinaryString(i));
+                    break;
+                case 8:
+                    System.out.println(Long.toOctalString(i));
+                    break;
+                case 10:
+                    System.out.println(i);
+                    break;
+                case 16:
+                    System.out.println(Long.toHexString(i));
+                    break;
+                default:
+                    System.out.println(i);
+
+            }
+            sleep(millis);
         }
+        System.out.println("### YOU HAVE REACHED THE FINAL COUNTDOWN ###");
     }
+
+
 }
