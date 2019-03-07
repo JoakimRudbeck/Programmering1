8264ad56906618dca437cc4b0abdbedfe594dcb4
sbu470 <sbu470@edu.sollentuna.se>
2019-03-07 10:54:03 +0100

    update

--- src/kapitelX_Enhetstester/KlassSomSkaTestas.java
+++ src/kapitelX_Enhetstester/KlassSomSkaTestas.java
@@ -3,7 +3,6 @@ package kapitelX_Enhetstester;
 public class KlassSomSkaTestas {
 
     static int kvadrera(int x){
-        int resultat = x*x;
-        return resultat;
+        return x*x;
     }
 }
