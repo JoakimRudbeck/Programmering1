30ddbd150230917f8273240d7ea20ead1566e4c2
sbu470 <sbu470@edu.sollentuna.se>
2019-03-12 08:13:19 +0100

    update

--- src/kapitel09_Metoder/Slump.java
+++ src/kapitel09_Metoder/Slump.java
@@ -1,8 +1,13 @@
 package kapitel09_Metoder;
 
+import java.util.Scanner;
+
 public class Slump {
 
     public static void main(String[] args) {
+        while(true){
+            System.out.println((int) (1 + Math.random()*6));
+        }
     }
 
     static void slumpaTal(){
@@ -15,5 +20,19 @@ public class Slump {
         // Math.random() ger värden mellan 0 och 1
         // Math.random()*k ger värden mellan 0 och k (dock lite mer spritt).
 
+    }
+    static void simuleraTärningskast() {
+        System.out.println("Hur många gånger ska vi kasta tärningarna?");
+        Scanner input = new Scanner(System.in);
+        int antalKast = input.nextInt();
+        int summa10 = 0;
+        for(int i = 0; i < antalKast; i++){
+            int summa = ((int)(1 + Math.random()*6)) + ((int)(1 + Math.random()*6)) + ((int)(1 + Math.random()*6));
+            if(summa == 10){
+                summa10++;
+            }
+        }
+        double svar = (double)summa10/antalKast;
+        System.out.println("Sannolikheten: " + svar);
     }
 }
