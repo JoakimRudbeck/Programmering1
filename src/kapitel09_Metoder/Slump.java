a6bafc08ab27e4d6d11f4cd078d096e0365ddf73
sbu470 <sbu470@edu.sollentuna.se>
2019-03-15 09:51:46 +0100

    update

--- src/kapitel09_Metoder/Slump.java
+++ src/kapitel09_Metoder/Slump.java
@@ -3,18 +3,46 @@ package kapitel09_Metoder;
 public class Slump {
 
     public static void main(String[] args) {
-
+        slumpaTal();
+        //slumpa4ever();
+        simuleraYatzy();
     }
 
     static void slumpaTal(){
         // Slumpa tal mellan 0 och 1 (1 exkl.)
         double slump1 = Math.random();
+        System.out.println(slump1);
 
         // Slumpa tal mellan 1 och 6
-        double slump2 = (int) (1 + Math.random()*6);
+        int slump2 = (int) (1 + Math.random()*6);
+        System.out.println(slump2);
 
         // Math.random() ger värden mellan 0 och 1
         // Math.random()*k ger värden mellan 0 och k (dock lite mer spritt).
 
     }
+
+    static void slumpa4ever(){
+        while(true){
+            System.out.println(Math.random());
+        }
+    }
+
+
+    static void simuleraYatzy(){
+        int antalSimuleringar = 1000000;
+        int antalYatzy = 0;
+        for(int i = 0; i < antalSimuleringar; i++){
+            int tärning1 = (int) (1 + Math.random()*6);
+            int tärning2 = (int) (1 + Math.random()*6);
+            int tärning3 = (int) (1 + Math.random()*6);
+            int tärning4 = (int) (1 + Math.random()*6);
+            int tärning5 = (int) (1 + Math.random()*6);
+            if(tärning1 == tärning2 && tärning2 == tärning3 && tärning3 == tärning4 && tärning4 == tärning5){
+                antalYatzy++;
+            }
+        }
+        System.out.printf("Av %d kast blev det %d YATZY (%.2f procent)", antalSimuleringar, antalYatzy, 100*(float)antalYatzy/antalSimuleringar);
+    }
+
 }
--- src/kapitel13_Sortering_och_Sökning/Bubbelsortering.java
+++ src/kapitel13_Sortering_och_Sökning/Bubbelsortering.java
@@ -3,13 +3,23 @@ package kapitel13_Sortering_och_Sökning;
 public class Bubbelsortering {
 
     public static void main(String[] args) {
-        int[] osorterad = new int[]{3,7,2,1,0,-34,128,-4,7};
-        int[] sorterad = bubbelSortera(osorterad);
+        bubbelSortering();
+    }
+
+    private static void bubbelSortering() {
+        int[] osorterad = new int[]{3,7,2,1,0,-34,128,-4,7};    // Osorterad lista
         String utdata = "";
-        for(int i : sorterad){
-            utdata +="," + i;
+        for(int i = 0; i < osorterad.length; i++){
+            utdata +="," + osorterad[i];
         }
-        System.out.println(utdata.substring(1));
+        System.out.println("Listan innan: " + utdata.substring(1));
+
+        int[] sorterad = bubbelSortera(osorterad);              // Sorterad lista
+        utdata = "";
+        for(int i = 0; i < sorterad.length; i++){
+            utdata +="," + sorterad[i];
+        }
+        System.out.println("Listan efter: " + utdata.substring(1));
     }
 
     static int[] bubbelSortera(int[] lista){
