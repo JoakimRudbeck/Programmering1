b61683f0b34b77f81d07d76c76b8948e303e4457
sbu470 <sbu470@edu.sollentuna.se>
2019-03-19 13:27:23 +0100

    lägger till klassen om felhantering

---
+++ src/KapitelY_Felhantering/Felhantering.java
@@ -0,0 +1,57 @@
+package KapitelY_Felhantering;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Felhantering {
+
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //felhanteringExempel();
+        arrayExempel();
+    }
+
+    private static void arrayExempel() {
+        try{
+            System.out.println("Antal tal: ");
+            int antalTal = scan.nextInt();
+            int[] tal = new int[antalTal];
+            for(int i = 0; i < tal.length; i++){
+                System.out.println("Tal på plats " + i + ":");
+                tal[i] = scan.nextInt();
+            }
+
+            System.out.println("Vilket index vill du skriva ut?");
+            int index = scan.nextInt();
+            System.out.println(tal[index]);
+
+        }
+        catch(InputMismatchException error){
+            System.out.println("Skriv ett heltal, tack.");
+        }
+        catch(NegativeArraySizeException error){
+            System.out.println("Skriv ett positivt heltal, tack.");
+        }
+        catch(NumberFormatException error){
+            System.out.println("Skriv endast tal, tack.");
+        }
+        catch(ArrayIndexOutOfBoundsException error){
+            System.out.println("Index måste vara existerande.");
+        }
+    }
+
+    private static void felhanteringExempel() {
+        try{
+            scan = new Scanner(System.in);
+            System.out.println("Namn: ");
+            String namn = scan.nextLine();
+            System.out.println("Ålder: ");
+            int ålder = Integer.valueOf(scan.nextLine());
+            System.out.println("Du heter " + namn + " och är " + ålder + " år gammal.");
+        }
+        catch(NumberFormatException error){
+            System.out.println("Det har blivit fel med siffrorna, skriv ett tal tack.");
+        }
+    }
+}
