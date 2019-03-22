13442db46ae1f60bba52d8e10d4b39704cb980f9
sbu470 <sbu470@edu.sollentuna.se>
2019-03-22 10:22:35 +0100

    Merge branch 'master' of https://github.com/JoakimRudbeck/Programmering1
    # Conflicts:
    #	out/production/Programmering1/intelliJ_tips_och_trix.md
    #	out/production/Programmering1/kapitel07_Villkorssatser/Villkor.class
    #	out/production/Programmering1/kapitel09_Metoder/MatematiskaFunktionerExempel.class
    #	out/production/Programmering1/kapitel09_Metoder/MerExempel.class
    #	out/production/Programmering1/kapitel09_Metoder/Slump.class
    #	out/production/Programmering1/kapitel13_Sortering_och_Sökning/Bubbelsortering.class
    #	src/kapitel09_Metoder/Slump.java
    #	src/kapitel13_Sortering_och_Sökning/Bubbelsortering.java

---
+++ src/Prov_Kap_1_till_9/ProvKapEttTillNio_MALL.java
@@ -0,0 +1,66 @@
+package Prov_Kap_1_till_9;
+
+public class ProvKapEttTillNio_MALL {
+
+    public static void main(String[] args) {
+        uppgift1_2poäng();
+        uppgift2_3poäng();
+        uppgift3_4poäng();
+    }
+
+
+    private static void uppgift1_2poäng() {
+    /*
+        UPPGIFT 1: Produkten av tal                                         2 POÄNG
+
+        Fråga användaren efter två tal; t1 och t2.
+        Skapa en metod static int produktenAv((int tal1, int tal2) som returnerar produkten av tal1 och tal2.
+        Anropa din nyligen skapade metod och skriv en informerande text som anger
+        vad produkten är samt om produkten är ett jämnt eller ett udda tal.
+
+        Körexempel:
+        Skriv in ett tal, tack:
+        7
+        Skriv in ett till tal, tack:
+        9
+        7*9 är 63 vilket är ett udda tal.
+
+     */
+    }
+
+    private static void uppgift2_3poäng() {
+    /*
+        UPPGIFT 2: Kvadratutskrift                                          3 POÄNG
+
+        Använd någon typ av loop för att skriva ut de 10 första kvadraterna, d.v.s:
+        1 4 9 16 25 36 49 64 81 100
+
+     */
+    }
+
+    private static void uppgift3_4poäng() {
+    /*
+        UPPGIFT 3: Talbasbyte                                              4 POÄNG
+
+        Fråga användaren efter en talbas.
+        Fråga användaren efter ett tal T i denna talbas.
+        Fråga användaren vilken talbas hen vill omvandla de nyligen inskrivna talet T till.
+
+        Skapa en metod static int konverteraMellanBaser(int tal, int talbas)
+        Anropa den nyligen skapade metoden och skriv ut informerande text om vad resultatet blev.
+
+        Körexempel:
+        Tal?
+        14
+        Talbas?
+        10
+        Ny talbas?
+        2
+        14 i basen 10 är 10110 i basen 2.
+
+
+
+     */
+    }
+
+}
