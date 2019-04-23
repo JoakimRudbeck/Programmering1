6a3786d92af98c094343a97594ef8c9ea3579584
sbu470 <sbu470@edu.sollentuna.se>
2019-04-23 10:02:57 +0200

    exempel på grafisk input istället för Scanner

---
+++ src/GrafiskInput_iställetFörScanner.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GrafiskInput_iställetFörScanner {
+
+    public static void main(String[] args) {
+        String namn = JOptionPane.showInputDialog(null, "Namn?");
+        int ålder = Integer.valueOf(JOptionPane.showInputDialog(null, "Ålder?"));
+        String beskrivning = "Hej " + namn + ", " + ålder + " år.";
+        JOptionPane.showMessageDialog(null, beskrivning);
+    }
+}
