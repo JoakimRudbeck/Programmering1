d9e8a0f4a23d378b5b91c3c91449677420e00022
sbu470 <sbu470@edu.sollentuna.se>
2019-09-26 11:35:32 +0200

    Update

--- src/kapitel04_Operatorer/Övningar.java
+++ src/kapitel04_Operatorer/Övningar.java
@@ -8,6 +8,7 @@ public class Övningar {
     public static void main(String[] args) {
         övning4_2();
         övning4_4();
+        övning4_5();
     }
 
     private static void övning4_2() {
@@ -33,5 +34,16 @@ public class Övningar {
         System.out.print(--tal);  // denna rad minskar först tal till 3 (från 4) och sedan skriver ut tal som är 3.
         System.out.println(tal);  // denna rad skriver ut 3 eftersom tal nu är 3.
         // D.v.s. skriver ut 5433
+    }
+
+    private static void övning4_5() {
+        System.out.println("Övning 4.5");
+        int tal1 = 5 + 6%4;             // motsv 5 + 2 ty modulo går före plus.
+        System.out.println(tal1);       // skriver ut 7.
+        int tal2 = (5 + 6)%4;           // motsv 11 % 4 ty parentes går först.
+        System.out.println(tal2);       // skriver ut 3.
+        double tal3 = 5;
+        tal3 *= 2.5 + 1;                // motsv *= 3.5
+        System.out.println(tal3);       // skriver ut 17.5
     }
 }
