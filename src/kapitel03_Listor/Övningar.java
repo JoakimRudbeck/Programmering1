6367b7712063ff5c561fad182562010a678df783
sbu470 <sbu470@edu.sollentuna.se>
2019-09-26 10:40:21 +0200

    Update

--- src/kapitel03_Listor/Övningar.java
+++ src/kapitel03_Listor/Övningar.java
@@ -7,11 +7,26 @@ public class Övningar {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //övning3_1();
+        övning3_1();
         övning3_2();
     }
 
+    private static void övning3_1() {
+        System.out.println("Övning 3.1");
+        int[] minaTal = new int[4];
+        minaTal[0] = scanner.nextInt();
+        minaTal[1] = scanner.nextInt();
+        minaTal[2] = scanner.nextInt();
+        minaTal[3] = scanner.nextInt();
+        System.out.println("Talen i listan är:");
+        System.out.print(minaTal[0] + ",");
+        System.out.print(minaTal[1]+ ",");
+        System.out.print(minaTal[2]+ ",");
+        System.out.print(minaTal[3]+ ".");
+    }
+
     private static void övning3_2() {
+        System.out.println("Övning 3.2");
         int[] treTal = new int[3];
         System.out.println("Skriv in tal 1:");
         treTal[0] = scanner.nextInt();
@@ -28,18 +43,5 @@ public class Övningar {
         System.out.println(treTal[0]);
         System.out.println(treTal[1]);
         System.out.println(treTal[2]);
-    }
-
-    private static void övning3_1() {
-        int[] minaTal = new int[4];
-        minaTal[0] = scanner.nextInt();
-        minaTal[1] = scanner.nextInt();
-        minaTal[2] = scanner.nextInt();
-        minaTal[3] = scanner.nextInt();
-        System.out.println("Talen i listan är:");
-        System.out.print(minaTal[0] + ",");
-        System.out.print(minaTal[1]+ ",");
-        System.out.print(minaTal[2]+ ",");
-        System.out.print(minaTal[3]+ ".");
     }
 }
