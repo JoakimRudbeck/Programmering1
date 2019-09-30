9ddf4831ae7ab97ac7f13319a73ce2d72395351e
sbu470 <sbu470@edu.sollentuna.se>
2019-09-30 16:35:13 +0200

    Updating

--- src/kapitel04_Operatorer/Uppgifter.java
+++ src/kapitel04_Operatorer/Uppgifter.java
@@ -7,12 +7,15 @@ public class Uppgifter {
     public static void main(String[] args) {
         uppgift4_2();
         uppgift4_4();
+        uppgift4_5();
+        uppgift4_6();
+        uppgift4_7();
     }
 
     private static void uppgift4_2() {
         System.out.println("Uppgift 4.2");
         System.out.println("Skriv in ett heltal");
-        int tal = scanner.nextInt();
+        int tal = Integer.valueOf(scanner.nextLine());
         int ursprungliga = tal;
         tal++;
         tal*=2;
@@ -26,19 +29,58 @@ public class Uppgifter {
         }
     }
 
-
     static void uppgift4_4() {
         System.out.println("Skriv in ett fyrsiffrigt tal, tack.");
-        int mittTal = scanner.nextInt();
+        int mittTal = Integer.valueOf(scanner.nextLine());
         int mittTalJusterat = mittTal + 500;
         // lägger på 500 för att avrundningen ska bli rätt.
         int mittTalDelatPåTusen = (mittTalJusterat / 1000);
         // kom ihåg, int dividerat med int blir int, decimalerna försvinner.
         int mittTalAvrundat = mittTalDelatPåTusen * 1000; // Lägger på tre nollor
-        System.out.printf("Talet %d avrundat blir %d", mittTal, mittTalAvrundat);
+        System.out.printf("Talet %d avrundat blir %d\n", mittTal, mittTalAvrundat);
     }
 
+    private static void uppgift4_5() {
+        System.out.println("Uppgift 4.5");
+        System.out.println("Mata in ett bråktal på formen A/B");
+        String indataSträng = scanner.nextLine(); // t.e.x. "13/5"
+        String[] täljareOchNämnareSomSträngar = indataSträng.split("/");
+        // split fungerar såhär: Strängen delas upp där det finns ett "/" och en array skapas.
+        // motsvarar arrayen {"132, "5"}
+        int täljare = Integer.valueOf(täljareOchNämnareSomSträngar[0]);
+        int nämnare = Integer.valueOf(täljareOchNämnareSomSträngar[1]);
+        int hela = täljare / nämnare;
+        int rest = täljare % nämnare;
+        String svarIBlandadForm = hela + " " + rest + "/" + nämnare;
+        System.out.println("Talet " + täljare + "/" + nämnare + " blir i blandad form: " + svarIBlandadForm);
+    }
 
+    private static void uppgift4_6() {
+        System.out.println("Uppgift 4.6");
+        System.out.println("Ange antal timmar:");
+        int timmar = scanner.nextInt();
+        System.out.println("Ange antal minuter:");
+        int minuter = scanner.nextInt();
+        System.out.println("Ange antal sekunder:");
+        int sekunder = scanner.nextInt();
+        System.out.println("Tidsomvandling ger:");
+        double totTimmar = timmar + ((minuter*60 + sekunder)/3600.0);
+        double totMinuter = 60*timmar + minuter + sekunder/60.0;
+        int totSekunder = 60*60*timmar + 60*minuter + sekunder;
+        System.out.printf("%.3f h = %.1f min = %d s\n", totTimmar, totMinuter, totSekunder);
+    }
 
+    private static void uppgift4_7() {
+        System.out.println("Uppgift 4.7");
+        System.out.println("Ange tid i hela sekunder, tack:");
+        int tidISek = scanner.nextInt();
+        int tidISekOriginal = tidISek; // kopierar startvärdet för att ha till senare.
+        int timmar = tidISek / 3600;
+        tidISek-=timmar * 3600;
+        int minuter = tidISek / 60;
+        tidISek-=minuter * 60;
+        int sekunder = tidISek;
+        System.out.printf("%d sekunder = %d timmar, %d minuter, %d sekunder", tidISekOriginal, timmar, minuter, sekunder);
+    }
 
 }
